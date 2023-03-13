bfe80095163875be58652f565a6085435dc5587e
Davi Silva <davigsilva98@hotmail.com>
2023-03-12 21:49:07 -0300

    Replace Nested Conditional with Guard Clauses

--- src/main.ts
+++ src/main.ts
@@ -6,34 +6,36 @@ function isOvernight(date) {
 function isSunday(date) {
 	return date.getDay() === 0;
 }
+function isValidDistance(distance) {
+	return distance != null && distance != undefined && typeof distance === "number" && distance > 0;
+}
+function isValidDate(date) {
+	return date != null && date != undefined && date instanceof Date && date.toString() !== "Invalid Date";
+}
 
 export function calculateRides(segments) {
 	let fare = 0;
 	for (const segment of segments) {
-		if (segment.distance != null && segment.distance != undefined && typeof segment.distance === "number" && segment.distance > 0) {
-			if (segment.date != null && segment.date != undefined && segment.date instanceof Date && segment.date.toString() !== "Invalid Date") {
-				if (segment.date.getDate() === 1) {
-					fare += segment.distance * 1.5;
-				} else {
-					if (isOvernight(segment.date)) {
-						if (!isSunday(segment.date)) {
-							fare += segment.distance * 3.90;
-						} else {
-							fare += segment.distance * 5;
-						}
-					} else {
-						if (isSunday(segment.date)) {
-							fare += segment.distance * 2.9;
-						} else {
-							fare += segment.distance * 2.10;
-						}
-					}
-				}
+		if (!isValidDistance(segment.distance))
+			return -1;
+		if (!isValidDate(segment.date))
+			return -2;
+		if (segment.date.getDate() === 1) {
+			fare += segment.distance * 1.5;
+			continue;
+		}
+		if (isOvernight(segment.date)) {
+			if (!isSunday(segment.date)) {
+				fare += segment.distance * 3.90;
 			} else {
-				return -2;
+				fare += segment.distance * 5;
 			}
 		} else {
-			return -1;
+			if (isSunday(segment.date)) {
+				fare += segment.distance * 2.9;
+			} else {
+				fare += segment.distance * 2.10;
+			}
 		}
 	}
 	if (fare < 10) {
