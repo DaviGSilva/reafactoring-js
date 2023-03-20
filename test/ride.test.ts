589bca8ec86247620047bbc20a4a8dbf380c3b85
davi.silva <davi.silva@westwing.com.br>
2023-03-20 00:28:29 -0300

    Translate Tests

--- test/ride.test.ts
+++ test/ride.test.ts
@@ -1,26 +1,26 @@
 import Ride from '../src/Ride';
 
-test('Deve calcular uma corrida no primeiro dia do mês', function() {
+test('Should calculate a ride in first day of the month', function() {
     const ride = new Ride();
     ride.addSegment(10, new Date('2021-03-01T10:00:00'));
     expect(ride.calculateFare()).toBe(15);
 });
-test('Deve calcular uma corrida noturna', function() {
+test('Should calculate a night ride', function() {
     const ride = new Ride();
     ride.addSegment(10, new Date('2021-03-01T23:00:00'));
     expect(ride.calculateFare()).toBe(15);
 });
-test('Deve calcular uma corrida normal', function() {
+test('Should calculate a normal ride', function() {
     const ride = new Ride();
     ride.addSegment(10, new Date('2021-03-07T10:00:00'));
     expect(ride.calculateFare()).toBe(29);
 });
-test('Deve calcular uma corrida noturna no domingo', function() {
+test('Should calculate a night ride on Sunday', function() {
     const ride = new Ride();
     ride.addSegment(10, new Date('2021-03-07T23:00:00'));
     expect(ride.calculateFare()).toBe(50);
 });
-test('Deve calcular uma corrida com valor mínimo', function() {
+test('Should calculate a ride with minimum price', function() {
     const ride = new Ride();
     ride.addSegment(3, new Date('2021-03-01T10:00:00'));
     expect(ride.calculateFare()).toBe(10);
--- test/segment.test.ts
+++ test/segment.test.ts
@@ -1,13 +1,13 @@
 import Segment from '../src/Segment';
 
-test('Não deve existir segmento com distância inferior a zero', function() {
+test('should throw an error if segment distance less than zero', function() {
     expect(
         function() {
             new Segment(-10, new Date('2021-03-01T10:00:00'));
         }
     ).toThrow(new Error('Invalid distance'));
 });
-test('Não deve existir segmento com data inválida', function() {
+test('should throw an error if invalid segment date', function() {
     expect(
         function() {
             new Segment(10, new Date('abcdef'))
